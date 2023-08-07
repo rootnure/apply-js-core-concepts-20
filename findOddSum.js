628da9ab056af5133790cabcd019a73e7b1b04d1
rootnure <nur.diu.2791@gmail.com>
2023-08-07 19:45:12 +0600

    find odd sum

---
+++ findOddSum.js
@@ -0,0 +1,36 @@
+function getNumberArraySum(numbers) {
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const number = numbers[index];
+        sum += number;
+    }
+    return sum;
+}
+
+function getOddArrayFromFullArray(fullArrayOfNumbers) {
+    const oddArray = [];
+    for(let i = 0; i < fullArrayOfNumbers.length; i++) {
+        const index = i;
+        const value = fullArrayOfNumbers[index];
+        if(value % 2 === 0) {
+            continue;
+        }
+        else {
+            oddArray.push(value);
+        }
+    }
+    return oddArray;
+}
+
+function findOddSum(fullArray) {
+    const oddArray = getOddArrayFromFullArray(fullArray);
+    const oddArraySum = getNumberArraySum(oddArray);
+    return oddArraySum;
+}
+
+const arr = [32, 16, 58, 74, 96, 15, 68, 33, 17, 94, 6, 16];
+
+const oddSum = findOddSum(arr);
+
+console.log(oddSum);
