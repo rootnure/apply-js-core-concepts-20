fab3c151ecfee0dc59856890e9ba953161a7a6a6
rootnure <nur.diu.2791@gmail.com>
2023-08-07 14:20:21 +0600

    variable declaration: var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,9 @@
+var age = 15;
+var age = 20; // can redeclare same variable (may accidently override previous value) that declare by var
+
+let myAge = 11;
+myAge = 12; // can change the value of variable declared by let
+
+const countryName = 'Bangladesh';
+countryName = 'Chander desh'; // can not change the value of variable declare by const
+console.log(countryName);
